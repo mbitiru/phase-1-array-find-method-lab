bfc75c5b918a06fd40f7e08a30d3e0a7aa3004dc
tanveer.chege@student.moringaschool.com <tanveer.chege@student.moringaschool.com>
2022-08-22 20:45:11 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+//superbowlWin(record) returning year Denver Broncos won the superbowl and undefined if record has no win
+superbowlWin = (record) => {
+const result = record.find(records => records.result === "W");
+if (result) {
+    return result.year
+}
+}
